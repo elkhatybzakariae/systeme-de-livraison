16fde124a36d74b881ade04510adc7709c157c6d
abdelfatah errazy <errazy.abdelfatah@gmail.com>
2024-04-29 21:51:13 +0100

    updating the cdoe

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -199,7 +199,7 @@
                     <!--begin:Menu link-->
                     <div class="menu-item">
                         <!--begin:Menu link-->
-                        <a class="menu-link" href="">
+                        <a class="menu-link" href="{{ route('colis.index') }}">
                             <span class="menu-icon">
                                 <!--begin::Svg Icon | path: icons/duotune/general/gen014.svg-->
                                 <span class="svg-icon svg-icon-2">
--- resources/views/pages/ville/index.blade.php
+++ resources/views/pages/ville/index.blade.php
@@ -1,15 +1,71 @@
 @extends('layouts.admin.admin')
 @section('content')
 
-<!-- Modal -->
 <div class="modal fade" id="kt_modal_new_target" tabindex="-1" aria-hidden="true">
-  <!-- Modal content -->
+  <div class="modal-dialog modal-dialog-centered mw-650px">
+    <div class="modal-content rounded">
+      <div class="modal-header pb-0 border-0 justify-content-end">
+        <div class="btn btn-sm btn-icon btn-active-color-primary" data-bs-dismiss="modal">
+          <span class="svg-icon svg-icon-1">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1" transform="rotate(-45 6 17.3137)" fill="currentColor" />
+              <rect x="7.41422" y="6" width="16" height="2" rx="1" transform="rotate(45 7.41422 6)" fill="currentColor" />
+            </svg>
+          </span>
+        </div>
+      </div>
+      
+      <div class="modal-body scroll-y px-10 px-lg-15 pt-0 pb-15">
+        <form id="kt_modal_new_target_form" method="POST" class="form" action="{{ route('villes.store') }}">
+          @csrf
+          <div class="mb-13 text-center">
+            <h1 class="mb-3">Ajouter  une ville</h1>
+          </div>
+          <div class="d-flex flex-column mb-8 fv-row">
+            <label class="d-flex align-items-center fs-6 fw-semibold mb-2">
+              <span class="required">Ref</span>
+              <i class="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="tooltip" title="Specify a target name for future usage and reference"></i>
+            </label>
+            <input type="text" class="form-control form-control-solid" placeholder="Enter ref ville" id="ref" name="ref" />
+          </div>
+          <div class="d-flex flex-column mb-8 fv-row">
+            <label class="d-flex align-items-center fs-6 fw-semibold mb-2">
+              <span class="required">Ville Name</span>
+              <i class="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="tooltip" title="Specify a target name for future usage and reference"></i>
+            </label>
+            <input type="text" class="form-control form-control-solid" placeholder="Enter Ville Name" id="villename" name="villename" />
+          </div>
+          <div class="d-flex flex-column mb-8 fv-row">
+            <label class="d-flex align-items-center fs-6 fw-semibold mb-2">
+              <span class="required">Zone</span>
+              <i class="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="tooltip" title="Specify a target name for future usage and reference"></i>
+            </label>
+            <select name="id_Z" id="id_Z" class="form-control form-control-solid">
+                @foreach ($zones as $item)
+                   <option value="{{$item->id_Z}}"> {{$item->zonename}} </option>
+                @endforeach
+            </select>
+          </div>
+         
+          <div class="text-center">
+            <button type="reset" id="kt_modal_new_target_cancel" class="btn btn-light me-3">Cancel</button>
+            <button type="submit" id="kt_modal_new_target_submit" class="btn btn-primary">
+              <span class="indicator-label">Submit</span>
+              <span class="indicator-progress">Please wait...
+              <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </div>
+
+
 
 <div class="card card-flush">
   <div class="card-header align-items-center py-5 gap-2 gap-md-5">
     <div class="card-title">
-      <!-- Search and filter options -->
       <div class="d-flex align-items-center position-relative my-1">
         <span class="svg-icon svg-icon-1 position-absolute ms-4">
           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -26,10 +82,10 @@
           <option></option>
           <option value="all">All</option>
           <option value="1">Active</option>
-          <option value="0">Inactive</option>
+					<option value="0">Inactive</option>
         </select>
       </div>
-      <a class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#kt_modal_new_target">Ajouter un Colis</a>
+      <a  class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#kt_modal_new_target">Ajouter une Ville</a>
     </div>
   </div>
 
@@ -43,57 +99,42 @@
             </div>
           </th>
           <th class="min-w-200px">Id</th>
-          <th class="min-w-100px">Code d'Envoi</th>
-          <th class="min-w-100px">Date d'Expedition</th>
-          <th class="min-w-100px">Telephone</th>
-          <th class="min-w-100px">Nom du Magasin</th>
-          <th class="min-w-100px">Etat</th>
-          <th class="min-w-100px">Status</th>
-          <th class="min-w-100px">Ville</th>
-          <th class="min-w-70px">Prix</th>
-          <th class="min-w-70px">Actions</th>
+          <th class=" min-w-100px">Ref</th>
+          <th class=" min-w-100px">Ville Name</th>
+          <th class=" min-w-100px">Zone</th>
+          <th class=" min-w-70px">Status</th>
+          <th class=" min-w-70px">Actions</th>
         </tr>
       </thead>
       <tbody class="fw-semibold text-gray-600">
-        @foreach ($colis as $index=>$coli)
+        @foreach ($villes as $index=>$ville)
           <tr>
             <td>
               <div class="form-check form-check-sm form-check-custom form-check-solid">
-                <input class="form-check-input" type="checkbox" value="{{ $coli->id_C }}" />
+                <input class="form-check-input" type="checkbox" value="{{ $ville->id_V }}" />
               </div>
             </td>
             <td>
               <div class="">
                 <div class="ms-5">
-                  <a href="" class="text-gray-800 text-hover-primary fs-5 fw-bold">{{ $coli->id_C }}</a>
+                  <a href="" class="text-gray-800 text-hover-primary fs-5 fw-bold" >{{ $ville->id_V }}</a>
                 </div>
               </div>
             </td>
             <td class="pe-0">
-              <span class="fw-bold" data-kt-ecommerce-product-filter="code">{{ $coli->code_d_envoi }}</span>
+              <span class="fw-bold" data-kt-ecommerce-product-filter="ref">{{ $ville->ref }}</span>
             </td>
             <td class="pe-0">
-              <span class="fw-bold" data-kt-ecommerce-product-filter="date">{{ $coli->date_d_expedition }}</span>
+              <span class="fw-bold" data-kt-ecommerce-product-filter="villename">{{ $ville->villename }}</span>
             </td>
             <td class="pe-0">
-              <span class="fw-bold" data-kt-ecommerce-product-filter="telephone">{{ $coli->client->telephone }}</span>
+              <span class="fw-bold" data-kt-ecommerce-product-filter="zonename">{{ $ville->zone->zonename }}</span>
             </td>
-            <td class="pe-0">
-              <span class="fw-bold" data-kt-ecommerce-product-filter="magasin">{{ $coli->client->nom_magasin }}</span>
+            <td class=" pe-0" data-order="{{ $ville->statut }}">
+              <div class="badge badge-light-danger">{{ $ville->statut }}</div>
             </td>
-            <td class="pe-0">
-              <span class="fw-bold" data-kt-ecommerce-product-filter="etat">{{ $coli->etat }}</span>
-            </td>
-            <td class="pe-0">
-              <span class="fw-bold" data-kt-ecommerce-product-filter="status">{{ $coli->status }}</span>
-            </td>
-            <td class="pe-0">
-              <span class="fw-bold" data-kt-ecommerce-product-filter="ville">{{ $coli->ville->villename }}</span>
-            </td>
-            <td class="pe-0">
-              <span class="fw-bold" data-kt-ecommerce-product-filter="prix">{{ $coli->prix }}</span>
-            </td>
-            <td>
+            <!--begin::Action=-->
+            <td class="">
               <a href="#" class="btn btn-sm btn-light btn-active-light-primary" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">Actions
                 <span class="svg-icon svg-icon-5 m-0">
                   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -103,10 +144,12 @@
               </a>
               <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-7 w-125px py-4" data-kt-menu="true">
                 <div class="menu-item px-3">
-                  <a onclick="openModal('{{ $coli->villename }}','{{ $coli->zonename }}','{{ $coli->ref }}','{{ route('colis.update',$coli->id_C) }}')" data-bs-toggle="modal" data-bs-target="#kt_modal_new_target" class="menu-link px-3">Edit</a>
+                  <a onclick="openModal('{{ $ville->villename }}','{{ $ville->zone->zonename }}','{{ $ville->ref }}','{{ route('villes.update',$ville->id_V) }}')"data-bs-toggle="modal" data-bs-target="#kt_modal_new_target"
+                     class="menu-link px-3">Edit</a>
                 </div>
                 <div class="menu-item px-3">
-                  <form action="{{ route('colis.destroy',$coli->id_C) }}" method="POST">
+                  <form action="{{ route('villes.destroy',$ville->id_V) }}" 
+                    method="POST">
                     @method('DELETE')
                     @csrf
                     <input type="submit" class="menu-link px-3 btn text-danger" data-kt-ecommerce-product-filter="delete_row" value="delete">
@@ -120,34 +163,72 @@
     </table>
   </div>
 </div>
-
-<!-- jQuery -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-<script>
-$(document).ready(function() {
-  // Filter by search input
-  $('[data-kt-ecommerce-product-filter="search"]').on('input', function() {
-    var searchText = $(this).val().toLowerCase();
-    filterTable(searchText);
+  <script>
+  $(document).ready(function() {
+    // Filter by search input
+    $('[data-kt-ecommerce-product-filter="search"]').on('input', function() {
+      var searchText = $(this).val().toLowerCase();
+      filterTable(searchText);
+    });
+
+    // Filter by status select
+    $('[data-kt-ecommerce-product-filter="status"]').on('change', function() {
+      var status = $(this).val();
+      filterTableByStatus(status);
+    });
+
+    // Function to filter table by search text
+    function filterTable(searchText) {
+  $('#kt_ecommerce_products_table tbody tr').each(function() {
+    var villename = $(this).find('[data-kt-ecommerce-product-filter="villename"]').text().toLowerCase();
+    var zonename = $(this).find('[data-kt-ecommerce-product-filter="zonename"]').text().toLowerCase();
+    var ref = $(this).find('[data-kt-ecommerce-product-filter="ref"]').text().toLowerCase();
+    if (villename.includes(searchText) || zonename.includes(searchText) || ref.includes(searchText)) {
+      $(this).show();
+    } else {
+      $(this).hide();
+    }
+  });
+}
+
+
+    // Function to filter table by status
+    function filterTableByStatus(status) {
+      if (status === 'all') {
+        $('#kt_ecommerce_products_table tbody tr').show();
+      } else {
+        $('#kt_ecommerce_products_table tbody tr').each(function() {
+          var zoneStatus = $(this).find('td:eq(3)').text().trim().toLowerCase();
+          if (zoneStatus === status) {
+            $(this).show();
+          } else {
+            $(this).hide();
+          }
+        });
+      }
+    }
   });
 
-  // Filter by status select
-  $('[data-kt-ecommerce-product-filter="status"]').on('change', function() {
-    var status = $(this).val();
-    filterTableByStatus(status);
-  });
+  function openModal(villename = '', zonename = '', ref = '', actionUrl = "{{ route('zone.store') }}") {
+    // Set the ville name input value
+    document.getElementById('villename').value = villename;
+    // Set the reference input value
+    document.getElementById('ref').value = ref;
+    // Set the selected zone in the dropdown
+    var zoneSelect = document.getElementById('id_Z');
+    console.log(zoneSelect);
+    for (var i = 0; i < zoneSelect.options.length; i++) {
+        if (zoneSelect.options[i].text === zonename) {
+            zoneSelect.selectedIndex = i;
+            break;
+        }
+    }
+    document.getElementById('kt_modal_new_target_form').action = actionUrl;
+    // document.getElementById('kt_modal_new_target_form').method = 'PUT';
+    // Show the modal
 
-  // Function to filter table by search text
-  function filterTable(searchText) {
-    // Filter table logic
   }
-
-  // Function to filter table by status
-  function filterTableByStatus(status) {
-    // Filter table by status logic
-  }
-});
-
 </script>
 @endsection
