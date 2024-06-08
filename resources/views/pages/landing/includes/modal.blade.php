aec88ceb2c874e5e9dbc8e6ce1ea01fec81f5dcc
abdelfatah errazy <errazy.abdelfatah@gmail.com>
2024-06-08 13:57:11 +0100

    adding some thing

--- resources/views/pages/landing/includes/modal.blade.php
+++ resources/views/pages/landing/includes/modal.blade.php
@@ -8,9 +8,9 @@
               <i class="bx bx-x"></i>
             </span>
           </button>
-          <h2 class="modal-title" id="myModalLabel2">
+          <h2 class="modal-title" style="background-color: #aaa;height:130px"  id="myModalLabel2">
             <a class="side-bar-header" href="">
-              <img src="{{ asset('storage/images/logo-side-bar.png') }}" class="logo-header" alt="Logo">
+              <img src="{{ asset('storage/images/l.png') }}" height="300" style="width: 400px" class="logo-header" alt="Logo">
               
             </a>
           </h2>
